c63113d73f9a82645e3e711cb9a6eb91607b2bcb
MajorWebDev <94580499+MajorWebDev@users.noreply.github.com>
2022-01-29 21:49:41 +0700

    Add files via upload

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="col-lg-12">
 			<h1>Dashboard</h1>
-			<p>Selamat datang <strong><?=$_SESSION['user']?></strong> di <strong>Ikanesia</strong></p>
+			<p>Selamat datang <strong><?=$_SESSION['user']?></strong> di <strong>Major Fish Store</strong></p>
 			<a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
         </div>
 	</div>

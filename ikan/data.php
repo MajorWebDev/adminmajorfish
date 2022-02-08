7bcc11e4246729ec1892330b1f01be632c7c211e
MajorWebDev <94580499+MajorWebDev@users.noreply.github.com>
2022-02-08 10:15:28 +0700

    edit style

--- ikan/data.php
+++ ikan/data.php
@@ -118,5 +118,5 @@
             }
             ?>
     </div>
-    <div class="d-block p-2 bg-dark text-white"><a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
+    <div class="float-left"><a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
 <?php include_once('../dashboard/_footer.php'); ?>

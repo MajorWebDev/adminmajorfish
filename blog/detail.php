e6e9d50303fd7e5b23104ba474a4e30e931fa798
MajorWebDev <94580499+MajorWebDev@users.noreply.github.com>
2022-02-21 11:37:34 +0700

    Add files via upload

---
+++ blog/detail.php
@@ -0,0 +1,24 @@
+<?php include_once('../dashboard/_header.php'); ?>
+    <?php 
+        $id_blog = $_GET["id_blog"];
+        $sql = "select * from blog WHERE id_blog = '$id_blog'";
+        $res = mysqli_query($con, $sql);
+        while ($data = mysqli_fetch_array($res)) {
+    ?>
+
+    <div class="box">
+        <h1><?= $data["judul"]?></h1>
+        <h5>
+            By : <?= $data["pembuat"]?>. <small class="text-muted">Upload pada <?=$data["tanggal"]?></small>
+        </h5>
+        <img src="../_asset/upload_gambar/<?= $data["gambar"]?>" class="img-fluid rounded mx-auto d-block">
+        <p><?= $data["isi"]?></p>
+    </div>
+    
+    <?php
+            }
+    ?>
+    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+<script src="../_asset/js/jquery.js"></script>
+<script src="../_asset/js/bootstrap.min.js"></script>
+<?php include_once('../dashboard/_footer.php'); ?>

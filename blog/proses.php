3371edae5de659634e778685ba95501da32d1035
MajorWebDev <94580499+MajorWebDev@users.noreply.github.com>
2022-02-21 11:11:27 +0700

    update

--- blog/proses.php
+++ blog/proses.php
@@ -29,46 +29,45 @@ if(isset($_POST['add'])) {
         }
     }
 } else if(isset($_POST['edit'])) {
-    $id_ikan = trim(mysqli_real_escape_string($con, $_POST['id_ikan']));
+    $id_blog = trim(mysqli_real_escape_string($con, $_POST['id_blog']));
     $filename = $_FILES['gambar']['name'];
-    $kategori = trim(mysqli_real_escape_string($con, $_POST['kategori']));
-    $nama = trim(mysqli_real_escape_string($con, $_POST['nama']));
-    $deskripsi = trim(mysqli_real_escape_string($con, $_POST['deskripsi']));
-    $harga = trim(mysqli_real_escape_string($con, $_POST['harga']));
-    $status = trim(mysqli_real_escape_string($con, $_POST['status']));
-    $rating = trim(mysqli_real_escape_string($con, $_POST['rating']));
+    $judul = trim(mysqli_real_escape_string($con, $_POST['judul']));
+    $pembuat = trim(mysqli_real_escape_string($con, $_POST['pembuat']));
+    $isi = trim(mysqli_real_escape_string($con, $_POST['isi']));
+    $tanggal = trim(mysqli_real_escape_string($con, $_POST['tanggal']));
 
     if($filename != "") {
+        $awal = "post";
         $rand = rand();
         $ekstensi =  array('png','jpg','jpeg','gif');
         $filebaru = $_FILES['gambar']['name'];
         $ukuran = $_FILES['gambar']['size'];
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
 		
-		$sql = "SELECT * FROM ikan WHERE id_ikan='$id_ikan'";
+		$sql = "SELECT * FROM blog WHERE id_blog='$id_blog'";
 		$query = mysqli_query($con, $sql) or die (mysqli_error());
 		$data = mysqli_fetch_array($query);
 		$patch = '../_asset/upload_gambar/'.$data['gambar'];
 		
         if(!in_array($ext,$ekstensi) ) {
-            header("location:edit.php?id_ikan=$id_ikan&alert=gagal_ekstensi");
+            header("location:edit.php?id_blog=$id_blog&alert=gagal_ekstensi");
         } else{
             if($ukuran < 1044070){	
-                $xx = $rand.'_'.$filebaru;
-                move_uploaded_file($_FILES['gambar']['tmp_name'], '../_asset/upload_gambar/'.$rand.'_'.$filebaru);
-                mysqli_query($con, "UPDATE ikan SET gambar = '$xx', kategori = '$kategori', nama = '$nama', deskripsi = '$deskripsi', harga = '$harga', status = '$status', rating = '$rating' WHERE id_ikan = '$id_ikan'") or die (mysqli_error($con));
-                header("location:edit.php?id_ikan=$id_ikan&alert=berhasil");
+                $xx = $awal.'-'.$rand.'_'.$filebaru;
+                move_uploaded_file($_FILES['gambar']['tmp_name'], '../_asset/upload_gambar/'.$awal.'-'.$rand.'_'.$filebaru);
+                mysqli_query($con, "UPDATE blog SET gambar = '$xx', judul = '$judul', pembuat = '$pembuat', isi = '$isi', tanggal = '$tanggal' WHERE id_blog = '$id_blog'") or die (mysqli_error($con));
+                header("location:edit.php?id_blog=$id_blog&alert=berhasil");
 				
 				if(file_exists($patch)){
 				unlink($patch);
 				}
             } else{
-                header("location:edit.php?id_ikan=$id_ikan&alert=gagal_ukuran");
+                header("location:edit.php?id_blog=$id_blog&alert=gagal_ukuran");
             }
         }
     } else {
-        mysqli_query($con, "UPDATE ikan SET kategori = '$kategori', nama = '$nama', deskripsi = '$deskripsi', harga = '$harga', status = '$status', rating = '$rating' WHERE id_ikan = '$id_ikan'") or die (mysqli_error($con));
-        header("location:edit.php?id_ikan=$id_ikan&alert=berhasil");
+        mysqli_query($con, "UPDATE blog SET judul = '$judul', pembuat = '$pembuat', isi = '$isi', tanggal = '$tanggal' WHERE id_blog = '$id_blog'") or die (mysqli_error($con));
+        header("location:edit.php?id_blog=$id_blog&alert=berhasil");
     }
 }
 ?>

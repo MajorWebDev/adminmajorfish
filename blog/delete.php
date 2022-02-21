d2bcb2d1c61a0f715f95a8dda47aaa883782f58c
MajorWebDev <94580499+MajorWebDev@users.noreply.github.com>
2022-02-21 09:18:29 +0700

    add delete

---
+++ blog/delete.php
@@ -0,0 +1,20 @@
+<?php
+require_once "../_config/config.php";
+if (!isset($_SESSION['user'])) {
+	echo "<script>window.location='".base_url('../auth/login.php')."';</script>";
+} 
+
+$id_ikan = $_GET["id_blog"];
+$sql = "SELECT * FROM blog WHERE id_blog='$id_blog'";
+$query = mysqli_query($con, $sql) or die (mysqli_error());
+$data = mysqli_fetch_array($query);
+$patch = '../_asset/upload_gambar/'.$data['gambar'];
+
+if(file_exists($patch)){
+	unlink($patch);
+};
+
+mysqli_query($con, "DELETE FROM blog WHERE id_blog = '$_GET[id_blog]'") or die (mysqli_error($con));
+echo "<script>window.location='data.php';</script>";
+?>
+

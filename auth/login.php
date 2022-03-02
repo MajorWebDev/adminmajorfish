8bc81b3ba88f514b8c7dccef963ca63713b29e90
MajorWebDev <94580499+MajorWebDev@users.noreply.github.com>
2022-03-02 08:43:10 +0700

    add dashboard in isset session user

--- auth/login.php
+++ auth/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../_config/config.php";
 if (isset($_SESSION['user'])) {
-	echo "<script>window.location='dashboard';</script>";
+	echo "<script>window.location='../dashboard';</script>";
 } else {
 ?>
 
